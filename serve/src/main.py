a031332840722a80a9e639a6a498c7e2dc49335d
MaxTeselkin <tesmaxxxx@gmail.com>
2025-07-29 11:23:42 +0000

    experiment

--- serve/src/main.py
+++ serve/src/main.py
@@ -1,12 +1,16 @@
+import supervisely as sly
+
+sly.logger.info("Downloading mobileclip...")
+
 import subprocess
 
 subprocess.run(
     [
         "wget",
         "https://github.com/ultralytics/assets/releases/download/v8.3.0/mobileclip_blt.ts",
-    ]
+    ],
+    check=True,
 )
-import supervisely as sly
 from serve.src.yoloe_model import YOLOEModel
 from dotenv import load_dotenv
 
